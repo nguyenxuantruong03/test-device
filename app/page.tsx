1e4fddcc87bdc86b07b21868886a3766ffbfc0cd
Truong <nxt159753@gmail.com>
2024-06-10 09:21:05 +0700

    123

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,171 @@ import { useEffect, useState } from 'react';
 import { UAParser } from 'ua-parser-js';
 import DeviceDetector from 'device-detector-js';
 
+const appleDeviceLookup = {
+  // iPhone
+  "iPhone1,1": "iPhone",
+  "iPhone1,2": "iPhone 3G",
+  "iPhone2,1": "iPhone 3GS",
+  "iPhone3,1": "iPhone 4",
+  "iPhone3,2": "iPhone 4",
+  "iPhone3,3": "iPhone 4",
+  "iPhone4,1": "iPhone 4S",
+  "iPhone5,1": "iPhone 5",
+  "iPhone5,2": "iPhone 5",
+  "iPhone5,3": "iPhone 5c",
+  "iPhone5,4": "iPhone 5c",
+  "iPhone6,1": "iPhone 5s",
+  "iPhone6,2": "iPhone 5s",
+  "iPhone7,1": "iPhone 6 Plus",
+  "iPhone7,2": "iPhone 6",
+  "iPhone8,1": "iPhone 6s",
+  "iPhone8,2": "iPhone 6s Plus",
+  "iPhone8,4": "iPhone SE (1st generation)",
+  "iPhone9,1": "iPhone 7",
+  "iPhone9,2": "iPhone 7 Plus",
+  "iPhone9,3": "iPhone 7",
+  "iPhone9,4": "iPhone 7 Plus",
+  "iPhone10,1": "iPhone 8",
+  "iPhone10,2": "iPhone 8 Plus",
+  "iPhone10,3": "iPhone X",
+  "iPhone10,4": "iPhone 8",
+  "iPhone10,5": "iPhone 8 Plus",
+  "iPhone10,6": "iPhone X",
+  "iPhone11,2": "iPhone XS",
+  "iPhone11,4": "iPhone XS Max",
+  "iPhone11,6": "iPhone XS Max",
+  "iPhone11,8": "iPhone XR",
+  "iPhone12,1": "iPhone 11",
+  "iPhone12,3": "iPhone 11 Pro",
+  "iPhone12,5": "iPhone 11 Pro Max",
+  "iPhone12,8": "iPhone SE (2nd generation)",
+  "iPhone13,1": "iPhone 12 Mini",
+  "iPhone13,2": "iPhone 12",
+  "iPhone13,3": "iPhone 12 Pro",
+  "iPhone13,4": "iPhone 12 Pro Max",
+  // iPad
+  "iPad1,1": "iPad",
+  "iPad2,1": "iPad 2",
+  "iPad2,2": "iPad 2",
+  "iPad2,3": "iPad 2",
+  "iPad2,4": "iPad 2",
+  "iPad3,1": "iPad (3rd generation)",
+  "iPad3,2": "iPad (3rd generation)",
+  "iPad3,3": "iPad (3rd generation)",
+  "iPad3,4": "iPad (4th generation)",
+  "iPad3,5": "iPad (4th generation)",
+  "iPad3,6": "iPad (4th generation)",
+  "iPad6,11": "iPad (5th generation)",
+  "iPad6,12": "iPad (5th generation)",
+  "iPad7,5": "iPad (6th generation)",
+  "iPad7,6": "iPad (6th generation)",
+  "iPad7,11": "iPad (7th generation)",
+  "iPad7,12": "iPad (7th generation)",
+  "iPad11,6": "iPad (8th generation)",
+  "iPad11,7": "iPad (8th generation)",
+   // iPad
+   "iPad2,5": "iPad mini",
+   "iPad2,6": "iPad mini",
+   "iPad2,7": "iPad mini",
+   "iPad4,4": "iPad mini 2",
+   "iPad4,5": "iPad mini 2",
+   "iPad4,6": "iPad mini 2",
+   "iPad4,7": "iPad mini 3",
+   "iPad4,8": "iPad mini 3",
+   "iPad4,9": "iPad mini 3",
+   "iPad5,1": "iPad mini 4",
+   "iPad5,2": "iPad mini 4",
+   "iPad5.6": "iPad mini 5",
+   "iPad5.5": "iPad mini 6",
+   "iPad11,1": "iPad mini (5th generation)",
+   "iPad11,2": "iPad mini (5th generation)",
+   // iPad Pro
+   "iPad6,3": "iPad Pro (9.7-inch)",
+   "iPad6,4": "iPad Pro (9.7-inch)",
+   "iPad6,7": "iPad Pro (12.9-inch)",
+   "iPad6,8": "iPad Pro (12.9-inch)",
+   "iPad7,1": "iPad Pro (12.9-inch, 2nd generation)",
+   "iPad7,2": "iPad Pro (12.9-inch, 2nd generation)",
+   "iPad7,3": "iPad Pro (10.5-inch)",
+   "iPad7,4": "iPad Pro (10.5-inch)",
+   "iPad8,1": "iPad Pro (11-inch)",
+   "iPad8,2": "iPad Pro (11-inch)",
+   "iPad8,3": "iPad Pro (11-inch)",
+   "iPad8,4": "iPad Pro (11-inch)",
+   "iPad8,5": "iPad Pro (12.9-inch, 3rd generation)",
+   "iPad8,6": "iPad Pro (12.9-inch, 3rd generation)",
+   "iPad8,7": "iPad Pro (12.9-inch, 3rd generation)",
+   "iPad8,8": "iPad Pro (12.9-inch, 3rd generation)",
+   "iPad8,9": "iPad Pro (11-inch, 2nd generation)",
+   "iPad8,10": "iPad Pro (11-inch, 2nd generation)",
+   "iPad8,11": "iPad Pro (12.9-inch, 4th generation)",
+   "iPad8,12": "iPad Pro (12.9-inch, 4th generation)",
+   "iPad13,4": "iPad Pro (11-inch, 3rd generation)",
+   "iPad13,5": "iPad Pro (11-inch, 3rd generation)",
+   "iPad13,6": "iPad Pro (11-inch, 3rd generation)",
+   "iPad13,7": "iPad Pro (11-inch, 3rd generation)",
+   "iPad13,8": "iPad Pro (12.9-inch, 5th generation)",
+   "iPad13,9": "iPad Pro (12.9-inch, 5th generation)",
+   "iPad13,10": "iPad Pro (12.9-inch, 5th generation)",
+   "iPad13,11": "iPad Pro (12.9-inch, 5th generation)",
+   // iPad Air
+   "iPad4,1": "iPad Air",
+   "iPad4,2": "iPad Air",
+   "iPad4,3": "iPad Air",
+   "iPad5,3": "iPad Air 2",
+   "iPad5,4": "iPad Air 2",
+   "iPad11,3": "iPad Air (3rd generation)",
+   "iPad11,4": "iPad Air (3rd generation)",
+   "iPad13,1": "iPad Air (4th generation)",
+   "iPad13,2": "iPad Air (4th generation)",
+    // Mac
+  "MacBook10,1": "MacBook (12-inch, Early 2015)",
+  "MacBookAir8,1": "MacBook Air (Retina, 13-inch, 2018)",
+  "MacBookAir8,2": "MacBook Air (Retina, 13-inch, 2018)",
+  "MacBookAir9,1": "MacBook Air (Retina, 13-inch, 2020)",
+  "MacBookAir9,2": "MacBook Air (Retina, 13-inch, 2020)",
+  "MacBookPro15,1": "MacBook Pro (15-inch, 2018)",
+  "MacBookPro15,2": "MacBook Pro (15-inch, 2018)",
+  "MacBookPro15,3": "MacBook Pro (15-inch, 2018)",
+  "MacBookPro15,4": "MacBook Pro (15-inch, 2018)",
+  "MacBookPro15,5": "MacBook Pro (15-inch, 2018)",
+  "MacBookPro15,6": "MacBook Pro (15-inch, 2018)",
+  "MacBookPro15,7": "MacBook Pro (15-inch, 2018)",
+  "MacBookPro16,1": "MacBook Pro (16-inch, 2019)",
+  "MacBookPro16,2": "MacBook Pro (16-inch, 2019)",
+  "MacBookPro16,3": "MacBook Pro (13-inch, 2020)",
+  "MacBookPro16,4": "MacBook Pro (13-inch, 2020)",
+  "MacBookPro16,5": "MacBook Pro (13-inch, 2020)",
+  "MacBookPro16,6": "MacBook Pro (13-inch, 2020)",
+  "MacBookPro16,7": "MacBook Pro (13-inch, 2020)",
+  "MacBookPro17,1": "MacBook Pro (13-inch, 2021, M1)",
+  "MacBookPro17,2": "MacBook Pro (13-inch, 2021, M1)",
+  "MacBookPro17,3": "MacBook Pro (13-inch, 2021, M1)",
+  "MacBookPro17,4": "MacBook Pro (14-inch, 2021, M1 Pro)",
+  "MacBookPro17,5": "MacBook Pro (14-inch, 2021, M1 Max)",
+  "MacBookPro17,6": "MacBook Pro (16-inch, 2021, M1 Pro)",
+  "MacBookPro17,7": "MacBook Pro (16-inch, 2021, M1 Max)",
+   // MacBook Pro M2
+  "MacBookPro18,1": "MacBook Pro (13-inch, 2023, M2)",
+  "MacBookPro18,2": "MacBook Pro (13-inch, 2023, M2)",
+  "MacBookPro18,3": "MacBook Pro (14-inch, 2023, M2)",
+  "MacBookPro18,4": "MacBook Pro (14-inch, 2023, M2)",
+  "MacBookPro18,5": "MacBook Pro (16-inch, 2023, M2)",
+  "MacBookPro18,6": "MacBook Pro (16-inch, 2023, M2)",
+  // MacBook Pro M3
+  "MacBookPro19,1": "MacBook Pro (13-inch, 2024, M3)",
+  "MacBookPro19,2": "MacBook Pro (13-inch, 2024, M3)",
+  "MacBookPro19,3": "MacBook Pro (14-inch, 2024, M3)",
+  "MacBookPro19,4": "MacBook Pro (14-inch, 2024, M3)",
+  "MacBookPro19,5": "MacBook Pro (16-inch, 2024, M3)",
+  "MacBookPro19,6": "MacBook Pro (16-inch, 2024, M3)",
+  // MacBook Air
+  "MacBookAir10,1": "MacBook Air (Retina, 13-inch, 2022, M2)",
+  "MacBookAir10,2": "MacBook Air (Retina, 13-inch, 2022, M2)",
+  "MacBookAir10,3": "MacBook Air (Retina, 13-inch, 2022, M3)",
+  "MacBookAir10,4": "MacBook Air (Retina, 13-inch, 2022, M3)"
+};
+
 interface DeviceInfo {
   type: string;
   brand: string;
@@ -31,22 +196,9 @@ const InfoDevice = () => {
 
     let fullModel = parsedDevice.device?.model || 'unknown';
 
-    // Handle detailed model information for various devices
     if (parsedDevice.device?.brand === 'Apple') {
-      if (/iPad/i.test(userAgent)) {
-        const match = userAgent.match(/(iPad; CPU OS [0-9_]+)/);
-        if (match && match[1]) {
-          fullModel = `iPad (${match[1].replace(/_/g, '.')})`;
-        }
-      } else if (/iPhone/i.test(userAgent)) {
-        const match = userAgent.match(/(iPhone; CPU iPhone OS [0-9_]+)/);
-        if (match && match[1]) {
-          fullModel = `iPhone (${match[1].replace(/_/g, '.')})`;
-        }
-      }
-    } else if (parsedDevice.device?.brand === 'Samsung') {
-      // Additional logic to get full model details for Samsung devices if needed
-      fullModel = parsedDevice.device?.model || 'Samsung Device';
+      const modelCode = parsedDevice.device?.model as keyof typeof appleDeviceLookup;
+      fullModel = appleDeviceLookup[modelCode] || modelCode || 'unknown Apple device';
     } else {
       fullModel = `${parsedDevice.device?.brand} ${parsedDevice.device?.model}` || 'unknown device';
     }
